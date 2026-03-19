d97649e038fac0e5eddf953a27f298ca3b2020ec
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-19 23:23:51 +0530

    simple but scalable distributions

--- distributions.py
+++ distributions.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+def generate_data(dist_type, n=100000):
+    if dist_type == "uniform":
+        data = np.sort(np.random.uniform(0, 1, n))
+    elif dist_type == "normal":
+        data = np.sort(np.random.normal(0.5, 0.1, n))
+    elif dist_type == "skewed":
+        data = np.sort(np.random.exponential(1, n))
+    return data
